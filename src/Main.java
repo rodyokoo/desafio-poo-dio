d282796de0d0482efc5149329794ccd697541d3c
rodyokoo <nihonrodrigo@hotmail.com>
2021-11-23 21:27:34 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,5 +23,52 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição bootcamp java developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria);
+
+        Dev devRodrigo = new Dev();
+        devRodrigo.setNome("Rodrigo");
+        devRodrigo.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteudos inscritos Rodrigo: ");
+        for(Conteudo rodrigoConteudoInscrito : devRodrigo.getConteudosInscritos()){
+            System.out.println("---- " + rodrigoConteudoInscrito);
+        }
+        devRodrigo.progredir();
+        devRodrigo.progredir();
+        devRodrigo.progredir();
+        System.out.println("Conteudos concluídos Rodrigo: ");
+        for(Conteudo rodrigoConteudoConcluido : devRodrigo.getConteudosConcluidos()){
+            System.out.println("---- " + rodrigoConteudoConcluido);
+        }
+        System.out.println("Conteudos inscritos Rodrigo: ");
+        for(Conteudo rodrigoConteudoInscrito : devRodrigo.getConteudosInscritos()){
+            System.out.println("---- " + rodrigoConteudoInscrito);
+        }
+        System.out.println("XP: " + devRodrigo.calcularTotalXp());
+        System.out.println("---------------");
+
+
+        Dev devFlavia = new Dev();
+        devFlavia.setNome("Flávia");
+        devFlavia.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteudos inscritos Flávia: ");
+        for(Conteudo flaviaConteudoInscrito : devFlavia.getConteudosInscritos()){
+            System.out.println("---- " + flaviaConteudoInscrito);
+        }
+        devFlavia.progredir();
+        System.out.println("Conteudos concluídos Flávia: ");
+        for(Conteudo flaviaConteudoConcluido : devFlavia.getConteudosConcluidos()){
+            System.out.println("---- " + flaviaConteudoConcluido);
+        }
+        System.out.println("Conteudos inscritos Flávia: ");
+        for(Conteudo flaviaConteudoInscrito : devFlavia.getConteudosInscritos()){
+            System.out.println("---- " + flaviaConteudoInscrito);
+        }
+        System.out.println("XP: " + devFlavia.calcularTotalXp());
     }
 }
